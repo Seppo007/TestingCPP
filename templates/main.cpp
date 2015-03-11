e034946a3030286419379862e9c88799a7e927bb
Sebastian Schalow <schalowseb@gmail.com>
2015-03-11 17:52:27 +0100

    Initial commit

---
+++ templates/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "templates.hpp"
+
+int main(){
+
+    double a = 55.12;
+    float b = 55.999f;
+
+    std::cout << "Maximum von " << a << " & " << b << ": " << tempMax(a, b) << std::endl;
+
+    std::cout << getaString(a) << std::endl;
+
+    myClass<std::string> klasse("Hallo Welt");
+    std::cout << "Attribut von Klasse hat den Inhalt: \"" << klasse.getAttrib() << "\"" << std::endl;
+
+    std::cout << "a ist vom Typ " << getTypeOf(a) << " und hat den Wert: " << a << std::endl;
+
+    return 0;
+
+}
---
+++ templates/templates.hpp
@@ -0,0 +1,62 @@
+#ifndef TEMPLATES
+#define TEMPLATES
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <typeinfo>
+
+template <typename T, typename U>
+T tempMax(T first, U second){
+    return (first > second) ? first : second;
+}
+
+template<typename T>
+std::string getaString(T var){
+    std::string str = "Uebergebenes Argument: ";
+    std::stringstream ret;
+    ret << str << var;
+    return ret.str();
+}
+
+template<typename T>
+std::string getTypeOf(T var){
+    std::string ret = typeid(var).name();
+
+    if(std::string(typeid(var).name()) == "i")
+        ret = "integer";
+
+    if(std::string(typeid(var).name()) == "d")
+        ret = "double";
+
+    if(std::string(typeid(var).name()) == "f")
+        ret = "float";
+
+    if(std::string(typeid(var).name()) == "c")
+        ret = "char";
+
+    if(std::string(typeid(var).name()) == "ss")
+        ret = "StringStream";
+
+    return ret;
+}
+
+template<typename T>
+class myClass{
+
+private:
+    T attrib;
+
+public:
+
+    // Constructor
+    myClass(T attribute){
+        attrib = attribute;
+    }
+
+    T getAttrib(){
+        return attrib;
+    }
+
+};
+
+#endif
